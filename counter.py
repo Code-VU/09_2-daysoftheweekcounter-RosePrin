e9bc95d9a0f1905a14bbfac4b53392f06f29c6cf
Sarah-Rose Prince <sarah-roseprince@Sarah-Roses-MacBook-Pro.local>
2023-03-29 20:20:40 -0500

    day of the week counter

--- counter.py
+++ counter.py
@@ -1,7 +1,25 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    try: 
+        file = open(file_name)
+    except:
+        print('invalid file name')
+        exit()
 
+    dayoftheweek_count = {}
+
+    for line in file:
+        if line.startswith('From '):
+            line_words = line.split()
+            week_day = line_words[2]
+            
+            if week_day not in dayoftheweek_count:
+                dayoftheweek_count[week_day] = 1
+            else:
+                dayoftheweek_count[week_day] += 1
+
+    print(dayoftheweek_count)
 
 
 ## if you want to test locally run > python payCalculator.py
